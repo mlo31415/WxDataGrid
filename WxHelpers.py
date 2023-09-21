c82cd53ed077663e2a2f40218ebd3f8eeaaa9f05
mlo31415 <mlo@baskerville.org>
2023-09-21 10:07:12 -0400

    Fix comment to be clearer

--- WxHelpers.py
+++ WxHelpers.py
@@ -86,11 +86,10 @@ class ProgressMsg(object):
         self.pm.Close()
 
 
-#
 # Returns True if processing should continue; False if it should end
 def OnCloseHandling(event, needssaving: bool, msg: str) -> bool:
     if needssaving:
-        if event is None or type(event) == wx._core.CommandEvent:  # When the close event is None or is an ESC or the ID_Cancel button, it's not a vetoable event, so it needs to be handled separately
+        if event is None or type(event) == wx._core.CommandEvent:  # When the close event is None or is an ESC or the ID_Cancel button, it's not a veto-able event, so it needs to be handled separately
             resp=wx.MessageBox(msg, 'Warning', wx.OK|wx.CANCEL|wx.ICON_WARNING)
             if resp == wx.CANCEL:
                 return True
