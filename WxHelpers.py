4edfe562b4fbc272f658329b696cf93ec1e6d220
mlo31415 <mlo@baskerville.org>
2024-07-18 21:18:02 -0400

    Fix comments

--- WxHelpers.py
+++ WxHelpers.py
@@ -62,7 +62,7 @@ class ProgressMessage2(object):
 #       ProgressMessage(parent).Show(message)       # Display a message, creating a popup dialog if needed
 #       ProgressMessage(parent).Close(delay=sec)    # Delay sec seconds and then close the progress message
 # It may also be used as:
-#   with ProgressMsg(parent, "message", delay=1 as msg:
+#   with ProgressMessage(parent, "message", delay=1 as msg:
 #       ...
 #       ...
 #       etc.
@@ -95,7 +95,7 @@ class ProgressMessage(object):
 
     def Close(self, delay: float=0) -> None:
         if ProgressMessage._progressMessageDlg is None:
-            Log("ProgressMessage.Close() called without an existing ProgressDialo")
+            Log("ProgressMessage.Close() called without an existing ProgressDialog")
             return
 
         if delay > 0:
